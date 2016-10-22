b1c319b87cb7b7b76b7ada15a7d6a093e411ef87
Thiago Passos <thiago@passos.com.au>
2016-10-22 11:30:11 +1000

    steps 1,2,3,4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminPermissionsComponent } from './admin/admin-permissions.component';
+import { MaterialModule } from '@angular/material';
+import { routedComponents } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    AdminPermissionsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    routedComponents
   ],
   providers: [],
   bootstrap: [AppComponent]
